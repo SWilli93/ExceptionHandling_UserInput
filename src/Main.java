a92ad33abc3a6b13d744ba8dfcd43e30740ec004
Scott <ScottWilliamsSolutions@gmail.com>
2023-09-28 20:00:33 -0400

    Init + Methods for user input .Console and Scanner

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, What's your Name? ");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+        String dateOfBirth = System.console().readLine("What year were you born?");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return " So you are " + age + " years old";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, Whats your Name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
+    }
+
+    public static int checkData(int currentYear, String dateOfBirth) {
+
+        int dob = Integer.parseInt(dateOfBirth);
+        int minimumYear = currentYear - 125;
+
+        if ((dob < minimumYear) || (dob > currentYear)) {
+            return -1;
+        }
+        return (currentYear - dob);
+    }
+}
