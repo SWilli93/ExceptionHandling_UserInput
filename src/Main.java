0c4a1cc75902f64ef880f6420680087f38a65b84
Scott <ScottWilliamsSolutions@gmail.com>
2023-09-28 20:17:07 -0400

    Added Exception handling and validity checking to scanner method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        int currentYear = 2023;
+
+        // Try block because System.Console wont work in IDEs
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            System.out.println(getInputFromScanner(currentYear));
+        }
+
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -25,14 +34,27 @@ public class Main {
         System.out.println("Hi " + name + ", Thanks for taking the course!");
 
         System.out.println("What year were you born? ");
-        String dateOfBirth = scanner.nextLine();
-        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        boolean validDob = false;
+        int age = 0;
+
+        // While Loop with exception handling to make sure correct birth years input
+        do {
+            System.out.println("Enter a year of birth >= " + (currentYear - 125) + " and <= " + (currentYear));
+            try {
+                age = checkData(currentYear, scanner.nextLine());
+                validDob = age < 0 ? false : true; // validating date of birth
+            } catch (NumberFormatException badUserData) {
+                System.out.println("Characters not allowed! Try again.");
+            }
+
+        } while (!validDob);
 
         return "So you are " + age + " years old";
     }
 
     public static int checkData(int currentYear, String dateOfBirth) {
-
+        // check to make sure date of birth is within range
         int dob = Integer.parseInt(dateOfBirth);
         int minimumYear = currentYear - 125;
 
